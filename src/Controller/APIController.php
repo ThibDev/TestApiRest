43adf5b688060783574f79902a78bd9808b57ffa
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-18 11:33:54 +0200

    add createUser

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/api', name: 'api_')]
 class APIController extends AbstractController
@@ -32,14 +32,30 @@ class APIController extends AbstractController
         }
     }
 
-    
-    
-    #[Route('/utilisateurs/{user}', name: 'deleteUser', methods: ['DELETE'])]
-    public function deleteBook(User $user, EntityManagerInterface $em): JsonResponse 
+    #[Route('/utilisateurs/delete/{user}', name: 'deleteUser', methods: ['DELETE'])]
+    public function deleteUser(User $user, EntityManagerInterface $em): JsonResponse 
     {
         $em->remove($user);
         $em->flush();
 
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/utilisateurs/ajout', name:"createUser", methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse 
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        $content = $request->toArray();
+     
+        $em->persist($user);
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json');
+
+        $location = $urlGenerator->generate('detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 }
