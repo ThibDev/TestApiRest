2de4e343c68df9b477ba09362ef670b5e73d1552
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-18 10:49:02 +0200

    add deleteUser

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,11 +25,21 @@ class APIController extends AbstractController
     }
 
     #[Route('/utilisateurs/{user}', name: 'detailUser', methods: ['GET'])]
-    public function getDetailUser(User $user, SerializerInterface $serializer): JsonResponse {
-
-        {
+    public function getDetailUser(User $user, SerializerInterface $serializer): JsonResponse
+    { {
             $jsonUser = $serializer->serialize($user, 'json');
             return new JsonResponse($jsonUser, Response::HTTP_OK, ['accept' => 'json'], true);
         }
-   }
+    }
+
+    
+    
+    #[Route('/utilisateurs/{user}', name: 'deleteUser', methods: ['DELETE'])]
+    public function deleteBook(User $user, EntityManagerInterface $em): JsonResponse 
+    {
+        $em->remove($user);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
