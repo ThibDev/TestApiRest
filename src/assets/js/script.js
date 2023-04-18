818ce888ed6e6317d0ab10e4bbe35e70997416d9
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-18 09:47:20 +0200

    add bundles

---
+++ src/assets/js/script.js
@@ -0,0 +1,18 @@
+fetch("http://127.0.0.1:8000/api/utilisateurs/liste")
+.then(users =>{
+      return users.json();
+})
+.then(users =>{
+      users.forEach(users => {
+      const li = document.createElement("li");
+      li.textContent = users.lastname;
+      const li2 = document.createElement("li");
+      li2.textContent = users.firstname;
+      const li3 = document.createElement("li");
+      li3.textContent = users.mail;
+      document.getElementById("liste").appendChild(li);
+      document.getElementById("liste").appendChild(li2);
+      document.getElementById("liste").appendChild(li3);
+      li3.style.marginBottom = "20px"; 
+      });
+})
